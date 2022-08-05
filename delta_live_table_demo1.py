d40cf35e876f2ade87e5b4006c3cbef7c9bac544
xiongnanbin@126.com <xiongnanbin@126.com>
2022-08-05 07:16:11 +0000

    dlt demo
    dlt demo

---
+++ delta_live_table_demo1.py
@@ -0,0 +1,34 @@
+# Databricks notebook source
+# MAGIC %python
+# MAGIC import dlt
+# MAGIC from pyspark.sql.functions import *
+# MAGIC from pyspark.sql.types import *
+# MAGIC 
+# MAGIC data_path = "/databricks-datasets/retail-org/customers/"
+# MAGIC @dlt.table(
+# MAGIC   comment="retail-org customer dataset"
+# MAGIC )
+# MAGIC def cust_raw():
+# MAGIC   return (spark.read.format("csv").option("header", True).option("inferSchema", True).load(data_path))
+# MAGIC 
+# MAGIC @dlt.table(
+# MAGIC   comment="customer data cleaned and prepared for analysis."
+# MAGIC )
+# MAGIC @dlt.expect("valid_tax_id", "tax_id IS NOT NULL")
+# MAGIC @dlt.expect_or_drop("valid_reg_city", "region is not null")
+# MAGIC def cust_prepared():
+# MAGIC   return (
+# MAGIC     dlt.read("cust_raw")
+# MAGIC        .select("region", "city", "customer_id")
+# MAGIC   )
+# MAGIC 
+# MAGIC @dlt.table(
+# MAGIC   comment="customer group by region"
+# MAGIC )
+# MAGIC def cust_agg():
+# MAGIC   return (
+# MAGIC     dlt.read("cust_prepared")
+# MAGIC       .filter("city is null")
+# MAGIC       .groupBy("region")
+# MAGIC       .agg(countDistinct("customer_id").alias("cust_cnt"))
+# MAGIC   )
--- demo.sql
+++ demo.sql
@@ -1,5 +1,18 @@
 -- Databricks notebook source
--- MAGIC %sql
--- MAGIC use bronze;
--- MAGIC 
--- MAGIC show tables;
+-- MAGIC %fs
+-- MAGIC ls /databricks-datasets/retail-org/customers/
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC data_path = "/databricks-datasets/retail-org/customers/"
+-- MAGIC # df = spark.read.csv(path=data_path, header=True, inferSchema=True)
+-- MAGIC df = spark.read.format("csv").option("header", True).option("inferSchema", True).load(data_path)
+-- MAGIC display(df)
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC from pyspark.sql.functions import *
+-- MAGIC df2 = df.groupBy("region").agg(countDistinct("customer_id").alias("cust_cnt"))
+-- MAGIC display(df2)
